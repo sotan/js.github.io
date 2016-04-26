dd9215ee90694452fe9e24caad3c90f48e2594cc
sotan <sotan@users.noreply.github.com>
2016-04-26 20:02:45 +0800

    Update config.js

--- config/config.js
+++ config/config.js
@@ -2,8 +2,8 @@ var CONFIG = CONFIG || {};
 CONFIG.prompt = function(cwd, user) {
    if (user)
       return '<span class="user">' + user +
-          '</span>@<span class="host">sotan.github.io</span>:<span class="cwd">' +
+          '</span>@<span class="host">js.github.io</span>:<span class="cwd">' +
           cwd + '</span>$ ';
-   return 'sotan.github.io 1.0 $ ';
+   return 'js.github.io 1.0 $ ';
 };
 CONFIG.username = '';
